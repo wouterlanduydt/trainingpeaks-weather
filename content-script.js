f83dc87f03be20782c17b0d40c5f185d009d767a
Wouter Landuydt <wouter.landuydt@gmail.com>
2022-03-09 20:12:28 +0100

    show weather data per day

--- content-script.js
+++ content-script.js
@@ -4,24 +4,38 @@ const LOCATION = "Centrum"
 const fetchWeatherData = async () => {
   return await fetch(ENDPOINT)
     .then(r => r.json())
-    .then(data => data.forecasts.filter(forecast => forecast.location === LOCATION))
+    .then(data => data.forecasts.filter(forecast => forecast.location === LOCATION && forecast.timeRange === 24))
+}
+
+const insertWeatherData = (weatherData) => {
+  weatherData.forEach(day => {
+    const date = day.beginDate.substring(0, 10)
+    const dateContainer = document.querySelectorAll(`[data-date="${date}"]`)[0]
+    const dateTitle = dateContainer.getElementsByClassName("dayNumberText")[0]
+
+    const id = `weather-${date}`
+
+    if (!!document.getElementById(id)) return
+
+    const weatherNode = `
+      <div id="${id}">
+        <span class="icon-weather-type icon-weather-type--${day.weathertype}"></span>
+        <span>${day.temperature.minimum} - ${day.temperature.maximum}</span>
+        <span class="icon-winddirection icon-winddirection--${day.wind.direction.toLowerCase()}"></span>
+        <span>${day.wind.speed1}</span>
+      </div>
+    `
+
+    dateTitle.insertAdjacentHTML('afterend', weatherNode);
+  });
 }
 
 (async () => {
   const weatherData = await fetchWeatherData()
-  console.log({ weatherData })
 
   var obs = new MutationObserver(() => {
-    const date = "2022-03-10"
-
-    const dateContainer = document.querySelectorAll(`[data-date="${date}"]`)[0];
-    const dateTitle = dateContainer.getElementsByClassName("dayNumberText")[0]
-    console.log({ dateContainer, dateTitle })
-
-    // TODO: check if element already exists
-    dateTitle.insertAdjacentHTML('afterend', '<div>Sample Div</div>');
+    insertWeatherData(weatherData)
   });
 
   obs.observe(document.body, { childList: true });
-
 })();
